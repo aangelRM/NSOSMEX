0ee988e0ed734d65fd90631aa5ecfb169187a830
aangelRM <angel58524410@gmail.com>
2023-11-23 16:53:38 -0600

    Render Static

--- APIANGEL/settings.py
+++ APIANGEL/settings.py
@@ -128,10 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
