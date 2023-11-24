a4b377cbbb024702dfa185a5e2917a040a63a779
aangelRM <angel58524410@gmail.com>
2023-11-24 13:36:46 -0600

    Render Static 5

--- APIANGEL/settings.py
+++ APIANGEL/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'APIANGEL/api/static')
+    os.path.join(BASE_DIR, 'api/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
